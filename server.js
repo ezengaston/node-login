0895cc6a1a3202a1b93c53d68511226dd07e298d
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-21 13:32:36 +0100

    Login logic added

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const bcrypt = require("bcrypt");
 const { v4 } = require("uuid");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+
+const initializePassport = require("./password-config");
+initializePassport(passport, (email) => {
+  return users.find((user) => user.email === email);
+});
 
 const app = express();
 
@@ -8,6 +20,17 @@ const users = [];
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -16,6 +39,15 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
