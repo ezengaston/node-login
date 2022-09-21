752541505069ff1c74855d3ceb405b194456e070
Ezen <82026690+ezengaston@users.noreply.github.com>
2022-09-21 12:38:49 +0100

    Added register logic

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
+const { v4 } = require("uuid");
 
 const app = express();
 
+const users = [];
+
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -14,6 +19,24 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const name = req.body.name;
+    const email = req.body.email;
+
+    users.push({
+      id: v4(),
+      name,
+      email,
+      password: hashedPassword,
+    });
+    res.redirect("/login");
+  } catch (e) {
+    res.redirect("/register");
+  }
 });
 
 app.listen(3005);
